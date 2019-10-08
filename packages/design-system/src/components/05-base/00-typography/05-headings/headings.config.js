aa23c19fe09ef97f45d4633eed5bc2e50f0b59ba
Matt Chapman <chapabu101@gmail.com>
2019-10-08 02:24:56 +0100

    Add heading and paragraph styles.

---
+++ packages/design-system/src/components/05-base/00-typography/05-headings/headings.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  default: 'h1',
+  collated: true,
+  variants: [
+    {
+      name: 'h1',
+      label: 'H1',
+      context: {
+        level: 1,
+        text: 'Heading 1',
+      },
+    },
+    {
+      name: 'h2',
+      label: 'H2',
+      context: {
+        level: 2,
+        text: 'Heading 2',
+      },
+    },
+    {
+      name: 'h3',
+      label: 'H3',
+      context: {
+        level: 3,
+        text: 'Heading 3',
+      },
+    },
+    {
+      name: 'h4',
+      label: 'H4',
+      context: {
+        level: 4,
+        text: 'Heading 4',
+      },
+    },
+    {
+      name: 'h5',
+      label: 'H5',
+      context: {
+        level: 5,
+        text: 'Heading 5',
+      },
+    },
+    {
+      name: 'h6',
+      label: 'H6',
+      context: {
+        level: 6,
+        text: 'Heading 6',
+      },
+    },
+  ],
+};
