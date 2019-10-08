d05c7b6f7e1ca789d648df50bcb744fb7d5f7409
Matt Chapman <chapabu101@gmail.com>
2019-10-08 01:46:32 +0100

    Fix Fractal settings.

--- packages/design-system/fractal.js
+++ packages/design-system/fractal.js
@@ -15,24 +15,24 @@ const twig = require('@frctl/twig')();
 
 fractal.components.engine(twig);
 fractal.components.set('ext', '.twig');
-// fractal.components.set('default.preview', '@preview');
+fractal.components.set('default.preview', '@preview');
 
 /**
  * SOURCES
  */
-fractal.components.set('path', path.join(__dirname, 'src/components'));
+fractal.components.set('path', path.join(__dirname, 'src', 'components'));
 fractal.web.set('static.mount', '/assets');
 
 // Twig doesn't work for documentation pages, so we'll leave it as Nunjucks.
 // @see https://github.com/frctl/twig/issues/19
 fractal.docs.engine('@frctl/nunjucks');
-fractal.docs.set('path', path.join(__dirname, 'src/docs'));
-fractal.web.set('static.path', path.join(__dirname, 'dist'));
+fractal.docs.set('path', path.join(__dirname, 'src', 'docs'));
+fractal.web.set('static.path', path.join(__dirname, 'dist', 'assets'));
 
 /**
  * BUILD
  */
-fractal.web.set('builder.dest', path.join(__dirname, 'build'));
+fractal.web.set('builder.dest', path.join(__dirname, 'dist', 'fractal'));
 
 /**
  * SERVER
